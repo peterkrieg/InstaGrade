ee88ed8d6880dfb1863417b1b61516203d9b9d1f
Peter Krieg <pkrieg2@gmail.com>
2015-10-31 12:07:42 -0600

    started to get bower running, and set up for live server

---
+++ README.md
@@ -0,0 +1,3 @@
+# InstaGrade
+
+###### No server project for DevMountain.
---
+++ js/instaService.js
@@ -0,0 +1,77 @@
+angular.module('myApp')
+.service('instaService', instaFunc);
+function instaFunc($http, $q){
+
+
+
+
+	this.getInstaFeed= function(token){
+		var deferred = $q.defer();
+
+		var URL = 'https://api.instagram.com/v1/users/self/media/recent?access_token='+token+'&callback=JSON_CALLBACK';
+
+		// empty array that will hold objects of 45 picture objects, or however many
+		var userMedia = [];
+		var page = 1;
+		// first function call
+		return eachRequest(URL);
+		
+
+		function eachRequest(URL){
+			console.log(URL);
+			console.log('getting page:', page++);
+			$http({
+				method: 'JSONP',
+				url: URL
+			}).then(function(response){
+				console.log('then page:', page -1);
+				var pictures = response.data;
+				var parsedData = pictures.data;
+				var nextURL = pictures.pagination.next_url;
+
+				for(var i=0; i<parsedData.length; i++){
+					userMedia.push(parsedData[i]);
+				}
+
+				// last media request if no next_url property, means you're done
+				if(!pictures.pagination.next_url){
+					// alert('whats');
+					deferred.resolve(userMedia);
+				}
+				else{
+					console.log('next:', nextURL);
+					console.log('#photos:', userMedia.length);
+					// recursion each following time
+					eachRequest(nextURL+'&callback=JSON_CALLBACK');
+				}
+				// console.log(parsedData);
+				// console.log('length is'+parsedData.data.length);
+			},
+			function (error) {
+				console.log('ERROR',error);
+			});
+
+			return deferred.promise;
+		}
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
