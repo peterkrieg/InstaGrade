f697cfe417f81c4ddc3cac4b8ece62c297a7cf02
Peter Krieg <pkrieg2@gmail.com>
2015-11-05 21:23:52 -0700

    more bad solutions to redirect from browser..

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ui.router'])
+.config(function($urlRouterProvider, $stateProvider){
+
+	$stateProvider
+	.state('home', {
+		url: '/',
+		templateUrl: 'partials/home.html',
+		// controller: 'homeCtrl'
+	});
+
+	$urlRouterProvider.otherwise('/');
+
+
+
+
+
+
+
+
+
+
+
+});
+
