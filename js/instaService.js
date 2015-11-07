856780e1372323cb6341005a491b56f7ba8cc8c6
Peter Krieg <pkrieg2@gmail.com>
2015-11-06 17:29:40 -0700

    started analyzing data, and planning out what to report

--- js/instaService.js
+++ js/instaService.js
@@ -1,108 +1,43 @@
 angular.module('myApp')
 .service('instaService', instaFunc);
-function instaFunc($http, $q){
 
+function instaFunc($http, $q){
 
 	this.getInstaFeed= function(token){
 		var deferred = $q.defer();
-
 		var URL = 'https://api.instagram.com/v1/users/self/media/recent?access_token='+token+'&callback=JSON_CALLBACK';
-
 		// empty array that will hold objects of 45 picture objects, or however many
 		var userMedia = [];
-		var page = 1;
+		// var page = 1;
+
 		// first function call
 		return eachRequest(URL);
 
+
 		function eachRequest(URL){
-			console.log(URL);
-			console.log('getting page:', page++);
+			// console.log(URL);
+			// console.log('getting page:', page++);
 			$http({
 				method: 'JSONP',
 				url: URL
 			}).then(function(response){
-				console.log('then page:', page -1);
+				// console.log('then page:', page -1);
 				var pictures = response.data;
 				var parsedData = pictures.data;
 				var nextURL = pictures.pagination.next_url;
-
 				for(var i=0; i<parsedData.length; i++){
 					userMedia.push(parsedData[i]);
 				}
 
-
-				//___________________________________________________	
 				// last media request if no next_url property, means you're done
 				if(!pictures.pagination.next_url){
-					var counter = 0;
-					var likes = [];
-					// gets likes from all of user's photos, or 50 most recent, whichever case (to avoid hundreds of api calls, if users has lots of photos)
+					// get all likes
+					getLikes(userMedia, deferred);
+				}
 
-
-					// max number of photos, to avoid >100 api calls just to get like data
-					var numPhotosMax;
-					if(userMedia.length<=100){
-						numPhotosMax = userMedia.length;
-					}
-					else{
-						numPhotosMax = 100;
-					}
-
-
-					// numPhotosMax = 5;
-
-					for(var j=0; j<numPhotosMax; j++) {
-						var id = userMedia[j].id;
-						// console.log(id);
-						$http({
-							method: 'JSONP',
-							url: 'https://api.instagram.com/v1/media/'+id+'/likes?access_token='+token+'&callback=JSON_CALLBACK'
-						}).then(function(response){
-							// console.log(counter);
-							var likeArr = response.data;
-							// console.log(likeArr);
-							likes.push(likeArr);
-							likes.sort(function(a,b){return a.data.length-b.data.length;});
-							// console.log('likes array sorted is...', likes);
-							// console.log(userMedia[counter]);
-							userMedia[counter].likesFull = likeArr;
-							counter++;
-							// I cap off the photos to 50 to start off, to avoid so many api calls
-							if(likes.length===numPhotosMax){
-								console.log('sorted likes array is..', likes);
-								// console.log('finished likes is', likes);
-								userMedia.likes = likes;
-								// console.log('user media is', userMedia);
-								deferred.resolve(userMedia);
-							}
-							// counter++;  
-							// for when I'm using counter, instead of capping at 10 photos
-						});
-
-
-					}
-
-
-					// var id = userMedia[0].id;
-					// console.log(id);
-					// $http({
-					// 	method: 'JSONP',
-					// 	url: 'https://api.instagram.com/v1/media/'+id+'/likes?access_token='+token+'&callback=JSON_CALLBACK'
-					// }).then(function(response){
-					// 	var likes = response.data;
-					// 	userMedia.push(likes);
-					// 	console.log('like media likes is', likes);
-					// 	console.log(userMedia);
-
-
-					// 	deferred.resolve(userMedia);
-
-					// });
-
-}
-else{
-	console.log('next:', nextURL);
-	console.log('#photos:', userMedia.length);
+				else{
+					console.log('next:', nextURL);
+					console.log('#photos:', userMedia.length);
 					// recursion each following time
 					eachRequest(nextURL+'&callback=JSON_CALLBACK');
 				}
@@ -113,9 +48,132 @@ else{
 				console.log('ERROR',error);
 			});
 
-return deferred.promise;
+			return deferred.promise;
+		}
+	};
+
+
+
+
+	//_________________________Getting Likes__________________________
+	function getLikes(userMedia, deferred){
+		var counter = 0;
+		var likes = [];
+
+		// max number of photos, to avoid >100 api calls just to get like data
+		var setMaxPhotos = 5;
+		var numPhotosMax;
+		if(userMedia.length<=setMaxPhotos){
+			numPhotosMax = userMedia.length;
+		}
+		else{
+			numPhotosMax = setMaxPhotos;
+		}
+
+		for(var j=0; j<numPhotosMax; j++) {
+			var id = userMedia[j].id;
+			// console.log(id);
+			$http({
+				method: 'JSONP',
+				url: 'https://api.instagram.com/v1/media/'+id+'/likes?access_token='+token+'&callback=JSON_CALLBACK'
+			}).then(function(response){
+				// console.log(counter);
+				var likeArr = response.data;
+				// console.log(likeArr);
+				likes.push(likeArr);
+				likes.sort(function(a,b){return a.data.length-b.data.length;});
+				// console.log('likes array sorted is...', likes);
+				userMedia[counter].likesFull = likeArr;
+				counter++;
+				// I cap off the photos to 50 to start off, to avoid so many api calls
+				if(likes.length===numPhotosMax){
+					console.log('sorted likes array is..', likes);
+					// console.log('finished likes is', likes);
+					userMedia.likes = likes;
+					// console.log('user media is', userMedia);
+
+					analyzeData(userMedia, likes, deferred);
+
+
+					
+				}
+			});
+		}
+	}
+
+
+	function analyzeData(userMedia, likes, deferred){
+		var sumLikes = 0;
+		var selfLiked = 0;
+		var numPics = 0;
+		var numVids = 0;
+		for(var i=0; i<userMedia.length; i++){
+			var currentMedia = userMedia[i];
+			// if(i%10===0){
+			// 	console.log(currentMedia);
+			// }
+			console.log(currentMedia);
+			if(currentMedia.user_has_liked===true){
+				selfLiked++;
+			}
+
+			if(currentMedia.type==="image"){
+				numPics++;
+			}
+			else if(currentMedia.type==="video"){
+				numVids++;
+			}
+
+			sumLikes+=currentMedia.likes.count;
+
+		}
+		console.log('total number of likes is', sumLikes);
+		console.log('number of media is', userMedia.length);
+		console.log('averange number of likes is', sumLikes/userMedia.length);
+		console.log('number of times you have liked your own photo is', selfLiked);
+		console.log('numbef of pictures is ', numPics);
+		console.log('number of videos is ', numVids);
+
+
+		console.log(likes);
+
+
+
+		deferred.resolve(userMedia);
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-};
 
 
 
@@ -123,17 +181,3 @@ return deferred.promise;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
