fe19bd646062e72412ef59804bf918a4124c14fd
Peter Krieg <pkrieg2@gmail.com>
2015-11-09 10:20:39 -0700

    more work with styling, finished rough draft of home page

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,9 +1,16 @@
 angular.module('myApp')
 .controller('homeCtrl', ctrlFunc);
 
-function ctrlFunc($scope, instaService, $state){
+function ctrlFunc($scope, instaService, $state, tokenService){
 	// console.log('home controller fired');
 
+	$scope.fireDemo = function(){
+		// This demo runs on my access token, which could change in future, instagram
+		// doesn't guarantee they don't expire
+		var token = '1359984932.c4fe6f4.32721a77599f4b11b20c1f2ffcbedab2';
+		tokenService.setToken(token);
+		$state.go('summary');
+	};
 
 
 
@@ -29,7 +36,6 @@ function ctrlFunc($scope, instaService, $state){
 
 
 
-		
 
 
 }
