852ef27de69e84c1e86608be00604f96fd6b0794
Peter Krieg <pkrieg2@gmail.com>
2015-12-06 23:19:54 -0700

    got basic bar charts working

--- client/js/directives/barChart.js
+++ client/js/directives/barChart.js
@@ -5,8 +5,24 @@ angular.module('myApp')
 			$(function(){
 				var $barCharts = elem.find('div.bar');
 
+				// console.log(scope);
+				// console.log(scope.user);
 
-				console.log(scope.user);
+				var likesReceived = scope.user.likesReceived;
+				var likesGiven = scope.user.likesGiven;
+
+				if(likesReceived>likesGiven){
+					elem.find('.likes-received').css('width', '100%');
+					elem.find('.likes-given').css('width', (likesGiven/likesReceived)*100+'%');
+				}
+				else if(likesReceived<likesGiven){
+					elem.find('.likes-given').css('width', '100%');
+					elem.find('.likes-received').css('width', (likesReceived/likesGiven)*100+'%');
+				}
+				else if(likesReceived===likesGiven){
+					elem.find('.bar').css('width', '100%');
+				}
+
 
 
 
@@ -57,6 +73,6 @@ angular.module('myApp')
 			});
 
 
-		}
-	}
+}
+}
 })
