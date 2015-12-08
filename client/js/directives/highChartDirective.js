ed3f02b02b173d3610942e585a99c1769b168088
Peter Krieg <pkrieg2@gmail.com>
2015-12-07 18:54:07 -0700

    super strange error happening with updating highcharts graph data.. seems to replace all the hashtag counts.. totally confusing

--- client/js/directives/highChartDirective.js
+++ client/js/directives/highChartDirective.js
@@ -5,7 +5,7 @@ angular.module('myApp')
 			console.log(scope.report.analytics);
 
 			$(function(){
-				$(elem).highcharts({
+				var chart = {
 					chart: {
 						type: 'column',
 						style: {
@@ -69,8 +69,91 @@ angular.module('myApp')
 
 
 
-				});
-			});
+				};//end of chart definition
+
+				$(elem).highcharts(chart);
+
+				chart = $(elem).highcharts();
+				// console.log('new chart is', chart);
+
+				// end of highcharts setup
+
+				///////////////////////////////////////////////////
+				//  Events of clicking left or right button, 
+				//  Cycling through hashtags
+				///////////////////////////////////////////////////
+
+
+				console.log(scope.report.analytics.tagsGroupsCounts);
+
+				var tagsGroupsNames = scope.report.analytics.tagsGroupsNames;
+				var tagsGroupsCounts = scope.report.analytics.tagsGroupsCounts;
+				console.log(tagsGroupsCounts);
+
+				var $leftButton = $(elem).parent().find('i.cycle-left');
+				var $rightButton = $(elem).parent().find('i.cycle-right');
+
+				// console.log($leftButton);
+				// console.log($rightButton);
+
+				$rightButton.on('click', function(e){
+					console.log('clicked right');
+					console.log(tagsGroupsCounts);
+					console.log(scope.report.analytics.tagsGroupsCounts);
+					e.preventDefault();
+
+
+					scope.index++;
+					scope.report.analytics.currentTags = tagsGroupsNames[scope.index];
+					scope.report.analytics.currentTagsCounts = tagsGroupsCounts[scope.index];
+
+					console.log(tagsGroupsCounts);
+					console.log(scope.report.analytics.tagsGroupsCounts);
+
+					
+					// // update data of chart
+					chart.xAxis[0].setCategories(scope.report.analytics.currentTags);
+					chart.series[0].setData(scope.report.analytics.currentTagsCounts);
+
+					scope.noMoreLeft = false;
+					scope.$apply();
+
+					if(!tagsGroupsCounts[scope.index+1]){
+						scope.noMoreRight = true;
+						scope.$apply();
+					}
+
+				})// end of right button function
+
+				$leftButton.on('click', function(e){
+					console.log('index before is', scope.index);
+					e.preventDefault();
+					scope.index--;
+					console.log('index after is ', scope.index);
+					console.log(tagsGroupsCounts);
+					scope.report.analytics.currentTags = tagsGroupsNames[scope.index];
+					scope.report.analytics.currentTagsCounts = tagsGroupsCounts[scope.index];
+					console.log(scope.report.analytics.currentTagsCounts);
+
+					console.log(scope.report.analytics.currentTagsCounts);
+					
+					// update data of chart
+					chart.xAxis[0].setCategories(scope.report.analytics.currentTags);
+					chart.series[0].setData(scope.report.analytics.currentTagsCounts);
+
+					scope.noMoreRight = false;
+					scope.$apply();
+
+					if(!tagsGroupsCounts[scope.index-1]){
+						scope.noMoreLeft = true;
+						scope.$apply();
+					}
+
+				})// end of left button function
+
+
+
+				
 
 
 
@@ -85,6 +168,23 @@ angular.module('myApp')
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			});// end of jquery ready function, nothing below this 
 		} // link
 	} // return
 }) // whole directive
+
+
