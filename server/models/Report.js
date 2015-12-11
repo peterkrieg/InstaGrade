c58e46ab3134c724c5390d736a72f9ed5f96697b
Peter Krieg <pkrieg2@gmail.com>
2015-12-11 11:18:44 -0700

    finished report schema, with lots of Mixed, for when I want to include everything

--- server/models/Report.js
+++ server/models/Report.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Mixed = Schema.Types.Mixed;
 
 var ReportSchema = Schema({
 	media: [{
-		// caption: {type: String},
+		attribution: Mixed,
+		caption: Mixed,
+		// comments: Mixed,
 		comments: {
 			count: {type: Number},
 			data: [{
@@ -16,63 +19,84 @@ var ReportSchema = Schema({
 					username: {type: String}
 				}
 			}]
-		},
+		},// end of comments
 		created_time: {type: Number},
 		filter: {type: String},
-
-		// skipped down to type..
+		images: Mixed,
+		likes: {
+			count: {type: Number}
+		},
+		likesFull: Mixed,
+		link: {type: String},
+		location: Mixed,
+		tags: Mixed,
 		type: {type: String},
-		user_has_liked: {type: Boolean}
+		user_has_liked: {type: Boolean},
+		users_in_photo: Mixed,
+
+		// stuff that is unique to videos
+		safeurl: Mixed,
+		videos: Mixed
 	}], // end of media
+
 	relationships: {
 		followers: Array,
-		follows: Array
-	},
-	grade: {
-		adjustedAverageNumLikes: {type: Number},
+		follows: Array,
+		likesComparisonArr: Mixed,
+		uniqueFollowers: Mixed,
+		uniqueFollows: Mixed
+	}, // end of relationships
+
+
+	// just accept everything on grade, it won't be much data
+	grade: Mixed,
+
+	analytics: {
+		allTagsArr: Mixed,
+		allTimes: Mixed,
 		averageNumLikes: {type: Number},
-		likesRatio: {type: Number},
+		currentTags: Mixed,
+		currentTagsCounts: Mixed,
+		daysOfWeek: Mixed,
+		daysOfWeekArr: Mixed,
+		hashtagScatter: Mixed,
 		numLikesGiven: {type: Number},
 		numLikesReceived: {type: Number},
+		numPics: {type: Number},
+		numVids: {type: Number},
 		numSelfLikes: {type: Number},
-		selfLikesRatio: {type: Number},
-		userRatio: {type: Number}
-	},
-	analytics: {
-		numPics: {type: Number}
-	},
+		tagsGroupsCounts: Mixed,
+		tagsGroupsNames: Mixed
+	}, // end of analytics.
+
 	map: {
 		allLocations: [{
 			date: {type: Number},
 			instagramId: {type: Number},
 			latitude: {type: Number},
 			longitude: {type: Number},
-			name: {type: String}
+			name: {type: String},
+			pic: Mixed
 		}]
 	},
 
 
-
+	// user which pretty much is same as User schema, 
+	// except I leave out date joined and reports array
+	// I decided not to reference user here, because user could
+	// change bio/picture/any of their info over time,
+	// might be useful to keep track of that
 	user: {
-		
+		name: {type: String},
+		username: {type: String},
+		instagramId: {type: Number},
+		bio: {type: String},
+		website: {type: String},
+		profilePicture: {type: String},
+		numMedia: {type: Number, required: true},
+		numFollowers: {type: Number, required: true},
+		numFollows: {type: Number, required: true},
 	}
-
-
-
-
-	// {
-	// 	instagramId: {type: Number},
-	// 	name: {type: String},
-	// 	profilePicture: {type: String},
-	// 	bio: {type: String},
-	// 	website: {type: String},
-	// 	numMedia: {type: Number, required: true},
-	// 	numFollowers: {type: Number, required: true},
-	// 	numFollows: {type: Number, required: true},
-	// 	dateJoined: {type: Date, required: true, default: new Date()},
-
-
-	// }
 
 
 
