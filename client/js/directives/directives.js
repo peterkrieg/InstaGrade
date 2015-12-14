4b4f584c55a8184da222c96788af0f66de0628bc
Peter Krieg <pkrieg2@gmail.com>
2015-12-14 00:20:47 -0700

    more small css changes.  tried to vertically center comment text, still failing..

--- client/js/directives/directives.js
+++ client/js/directives/directives.js
@@ -41,6 +41,30 @@ angular.module('myApp')
 	}
 })
 
+// //___________Vertically Centering Media Comments_____________
+
+// angular.module('myApp')
+// .directive('verticalCenter', function(){
+// 	return {
+// 		link: function(scope, elem, attrs){
+// 			$(function(){
+				
+				
+// 				var $comment = $(elem);
+// 				var $commentText = $(elem).find('li');
+// 				console.log($comment.height());
+// 				console.log($commentText.height())
+// 				// center comment text with margins, with inline styling
+// 				var bottomHeight = ($comment.height() - $commentText.height())/2;
+
+
+// 				$commentText.css('bottom', bottomHeight);
+// 			});
+
+// 		}
+// 	}
+// })
+
 
 //_________________________Wow JS______________
 
